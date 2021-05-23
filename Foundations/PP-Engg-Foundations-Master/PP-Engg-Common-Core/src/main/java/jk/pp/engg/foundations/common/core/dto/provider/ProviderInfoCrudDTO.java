40b43ed7fd6b9fd1fe50d1257cdc95c5478c0b9d
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-22 20:47:13 -0400

    Stable Provides MS

--- Foundations/PP-Engg-Foundations-Master/PP-Engg-Common-Core/src/main/java/jk/pp/engg/foundations/common/core/dto/provider/ProviderInfoCrudDTO.java
+++
@@ -1,8 +0,0 @@
-package jk.pp.engg.foundations.common.core.dto.provider;
-
-import jk.pp.engg.foundations.common.core.domain.provider.IProviderInfo;
-import jk.pp.engg.foundations.common.core.dto.DomainCrudDTO;
-
-public class ProviderInfoCrudDTO extends DomainCrudDTO<IProviderInfo> {
-
-}
