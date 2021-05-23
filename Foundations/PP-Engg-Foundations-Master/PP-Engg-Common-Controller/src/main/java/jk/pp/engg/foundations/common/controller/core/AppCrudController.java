09ae110175d425fb1b02369f9b4e70814b183a47
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-23 18:01:04 -0400

    Spring Spring Boot @Controller where when we give RequestMapping(name= instead of RequestMapping(path= , it keeps IllegalStateException

--- Foundations/PP-Engg-Foundations-Master/PP-Engg-Common-Controller/src/main/java/jk/pp/engg/foundations/common/controller/core/AppCrudController.java
+++ Foundations/PP-Engg-Foundations-Master/PP-Engg-Common-Controller/src/main/java/jk/pp/engg/foundations/common/controller/core/AppCrudController.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import jk.pp.engg.foundations.common.domain.core.BaseDomain;
 import jk.pp.engg.foundations.common.domain.core.CrudResultDTO;
 import jk.pp.engg.foundations.common.domain.core.DomainCrudDTO;
 import jk.pp.engg.foundations.common.service.core.AppCrudService;
 
-public class AppCrudController<T extends BaseDomain, DTO extends DomainCrudDTO<T>> {
+public abstract class AppCrudController<T extends BaseDomain, DTO extends DomainCrudDTO<T>> {
 
 	private AppCrudService<T, DTO> crudService;
 
@@ -21,50 +22,50 @@ public class AppCrudController<T extends BaseDomain, DTO extends DomainCrudDTO<T
 		this.crudService = crudService;
 	}
 
-	@RequestMapping(path = "create")
+	@RequestMapping(path = "create", method = RequestMethod.POST)
 	public ResponseEntity<CrudResultDTO<T>> create(@RequestBody DTO crudDTO) throws Exception {
 
 		CrudResultDTO<T> result = this.crudService.create(crudDTO);
 		return ResponseEntity.ok(result);
 	}
 
-	@RequestMapping(path = "update")
+	@RequestMapping(path = "update", method = RequestMethod.POST)
 	public ResponseEntity<CrudResultDTO<T>> update(@RequestBody DTO crudDTO) throws Exception {
 		CrudResultDTO<T> result = this.crudService.update(crudDTO);
 		return ResponseEntity.ok(result);
 	}
 
-	@RequestMapping(path = "delete-by-pk")
+	@RequestMapping(path = "delete-by-pk", method = RequestMethod.POST)
 	public ResponseEntity<CrudResultDTO<T>> deleteByPK(@PathVariable Long pk) throws Exception {
 		CrudResultDTO<T> result = this.crudService.deleteByPK(pk);
 		return ResponseEntity.ok(result);
 	}
 
-	@RequestMapping(path = "delete-all")
+	@RequestMapping(path = "delete-all", method = RequestMethod.POST)
 	public ResponseEntity<CrudResultDTO<T>> deleteAll() throws Exception {
 		CrudResultDTO<T> result = this.crudService.deleteAll(null);
 		return ResponseEntity.ok(result);
 	}
 
-	//@RequestMapping(path = "find-by-pk/{pk}")
+	@RequestMapping(path = "find-by-pk/{pk}", method = RequestMethod.GET)
 	public ResponseEntity<CrudResultDTO<T>> findByPK(@PathVariable Long pk) throws Exception {
 		CrudResultDTO<T> result = this.crudService.findByPK(pk);
 		return ResponseEntity.ok(result);
 	}
 
-	@RequestMapping(path = "get-all")
+	@RequestMapping(path = "get-all", method = RequestMethod.GET)
 	public ResponseEntity<CrudResultDTO<T>> getAll() throws Exception {
 		CrudResultDTO<T> result = this.crudService.getAll(null);
 		return ResponseEntity.ok(result);
 	}
 
-	@RequestMapping(path = "get-all-by-criteria")
+	@RequestMapping(path = "get-all-by-criteria", method = RequestMethod.POST)
 	public ResponseEntity<CrudResultDTO<T>> getAllByCriteria(@RequestBody DTO crudDTO) throws Exception {
 		CrudResultDTO<T> result = this.crudService.getAllByCriteria(crudDTO);
 		return ResponseEntity.ok(result);
 	}
 
-	@RequestMapping(path = "populate-initial-data")
+	@RequestMapping(path = "populate-initial-data", method = RequestMethod.GET)
 	public ResponseEntity<CrudResultDTO<T>> populateInitialData() throws Exception {
 		List<DTO> domains = populateInitialDataDomais();
 
